bb0305e4bd4adfa457d3400007740480b34370ca
Sandydht <sandydwi75@gmail.com>
2024-06-09 16:18:40 +0700

    feat: add new materi initializing formal parameter

---
+++ bin/initializing_formal_parameter/initializing_formal_parameter.dart
@@ -0,0 +1,13 @@
+class Person {
+  String name = 'Guest';
+  String? address;
+  final String country = 'Indonesia';
+
+  Person(this.name, this.address) { }
+}
+
+void main() {
+  var person1 = Person('Sandy', 'Central Java');
+  print(person1.name);
+  print(person1.address);
+}
